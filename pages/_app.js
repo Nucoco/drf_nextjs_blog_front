884b8e0afd4ef3f51d90e44514b9558fb66844d7
Nucoco <yuuta11511997@gmail.com>
2021-07-22 23:59:07 +0900

    main

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
+import Head from "next/head";
+import Layout from "../components/layout/Layout";
+import { Provider } from "next-auth/client";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Provider session={pageProps.session}>
+			<Layout>
+				<Head>
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
+				</Head>
+				<Component {...pageProps} />
+			</Layout>
+		</Provider>
+	);
 }
 
-export default MyApp
+export default MyApp;
